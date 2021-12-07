a8357e037920b3392438f366c4ad8b25675fe4e9
lianfeihong <lianfeihong@bytedance.com>
2021-12-07 16:26:22 +0800

    add origin dsmil

--- projects/WSL/wsl/modeling/roi_heads/__init__.py
+++ projects/WSL/wsl/modeling/roi_heads/__init__.py
@@ -8,3 +8,4 @@ from .roi_heads_oicr import OICRROIHeads
 from .roi_heads_cmil import CMILROIHeads
 from .roi_heads_pcl import PCLROIHeads
 from .roi_heads_uwsod import UWSODROIHeads
+from .roi_heads_dsmil import DSMILROIHeads
